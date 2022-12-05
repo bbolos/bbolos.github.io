c544123ff426ea6df6f8bbfd995d02af36a142dd
Bram Los <71885836+bbolos@users.noreply.github.com>
2022-12-05 23:35:21 +0100

    some small updates

--- doris/js/index.js
+++ doris/js/index.js
@@ -1,5 +1,12 @@
 //alert("hoi")
+const pageItems = [];
 const categories = ["PORTFOLIO", "KUNST", "LESGEVEN"];
+
+pageItems.forEach(element => {
+    var categorie = element.cat;
+    categorie = categorie.toUpperCase();
+    categories.push(categorie)
+})
 
 const subNav = document.getElementsByClassName("subNav")[0];
 
@@ -11,3 +18,22 @@ categories.forEach(element => {
     subNavA.push(link);
     subNav.appendChild(link);
 });
+
+class pageItem {
+    constructor(id, cat, name, text, img){
+        this.id = id;
+        this.cat = cat;
+        this.name = name;
+        this.text = text;
+        this.img = img;
+    }
+
+    createPageItem(){
+        var cont = document.getElementsByClassName("content")[0];
+        var item = document.createElement("article");
+        
+        
+
+        cont.appendChild(item);
+    }
+}
