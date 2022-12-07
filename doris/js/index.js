7d695b21d5b023793cfc6211312b7b05c174d25a
Bram Los <71885836+bbolos@users.noreply.github.com>
2022-12-07 12:51:48 +0100

    created content items

--- doris/js/index.js
+++ doris/js/index.js
@@ -1,12 +1,40 @@
 //alert("hoi")
-const pageItems = [];
+class pageItem {
+    constructor(id, cat, name, text, img){
+        this.id = id;
+        this.cat = cat;
+        this.name = name.toUpperCase();
+        this.text = text;
+        this.img = img;
+    }
+
+    createPageItem(){
+        var cont = document.getElementsByClassName("content")[0];
+        var item = document.createElement("article");
+        var img = document.createElement("img");
+        img.setAttribute("src",this.img);
+        item.appendChild(img);
+        var b = document.createElement("b");
+        b.innerHTML = this.name;
+        item.appendChild(b);
+        var p = document.createElement("p");
+        p.innerHTML = this.text.slice(0,97) + "...";
+        item.appendChild(p);
+        cont.appendChild(item);
+    }
+}
+
+const test = new pageItem(1, "KUNST", "Test", "test ".repeat(50), "img/index.png")
+
+const pageItems = [test, test, test, test, test, test, test];
 const categories = ["PORTFOLIO", "KUNST", "LESGEVEN"];
 
+/*
 pageItems.forEach(element => {
     var categorie = element.cat;
     categorie = categorie.toUpperCase();
     categories.push(categorie)
-})
+})*/
 
 const subNav = document.getElementsByClassName("subNav")[0];
 
@@ -19,21 +47,7 @@ categories.forEach(element => {
     subNav.appendChild(link);
 });
 
-class pageItem {
-    constructor(id, cat, name, text, img){
-        this.id = id;
-        this.cat = cat;
-        this.name = name;
-        this.text = text;
-        this.img = img;
-    }
+pageItems.forEach(element => {
+    element.createPageItem();
+})
 
-    createPageItem(){
-        var cont = document.getElementsByClassName("content")[0];
-        var item = document.createElement("article");
-        
-        
-
-        cont.appendChild(item);
-    }
-}
